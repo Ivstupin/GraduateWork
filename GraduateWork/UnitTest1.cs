cf5a17097bbcc4caa1017ce8fdede20e70188492
Ivan Stupin <gognit65@gmail.com>
2024-04-09 23:55:52 +0500

    Add project files.

---
+++ GraduateWork/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace GraduateWork
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
